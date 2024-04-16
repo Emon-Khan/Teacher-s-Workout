fabc13f52aa7b725c084ec344edd466f6404d69a
Emon-Khan <rkemon94@gmail.com>
2024-04-16 22:53:23 +0600

    Added background image inside register form but got some error

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -14,6 +14,9 @@ import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.Window.Type;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
 
 public class Register extends JFrame {
 
@@ -57,36 +60,36 @@ public class Register extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Register");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(200, 11, 126, 37);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 26));
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Username");
-		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1.setBounds(33, 59, 85, 27);
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Email");
-		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1_1.setBounds(33, 97, 85, 27);
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1_1);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Password");
-		lblNewLabel_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1_1_1.setBounds(33, 135, 85, 27);
+		lblNewLabel_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1_1_1);
 		
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("Confirm Password");
-		lblNewLabel_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1_1_1_1.setBounds(33, 173, 159, 27);
+		lblNewLabel_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1_1_1_1);
 		
 		JButton btnNewButton = new JButton("SIGNUP");
+		btnNewButton.setBounds(348, 325, 115, 37);
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBackground(new Color(210, 0, 98));
 		btnNewButton.setFont(new Font("Georgia", Font.BOLD, 15));
-		btnNewButton.setBounds(348, 325, 115, 37);
 		contentPane.add(btnNewButton);
 		
 		textField = new JTextField();
@@ -112,18 +115,18 @@ public class Register extends JFrame {
 		contentPane.add(passwordField_2);
 		
 		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Height");
-		lblNewLabel_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1_1_1_1_1.setBounds(33, 211, 159, 27);
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1_1_1_1_1);
 		
 		JLabel lblNewLabel_1_1_1_1_1_1 = new JLabel("Weight");
-		lblNewLabel_1_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1_1_1_1_1_1.setBounds(33, 249, 159, 27);
+		lblNewLabel_1_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1_1_1_1_1_1);
 		
 		JLabel lblNewLabel_1_1_1_1_1_1_1 = new JLabel("Age");
-		lblNewLabel_1_1_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		lblNewLabel_1_1_1_1_1_1_1.setBounds(33, 287, 159, 27);
+		lblNewLabel_1_1_1_1_1_1_1.setFont(new Font("Arial", Font.BOLD, 12));
 		contentPane.add(lblNewLabel_1_1_1_1_1_1_1);
 		
 		passwordField_3 = new JPasswordField();
@@ -133,5 +136,15 @@ public class Register extends JFrame {
 		passwordField_4 = new JPasswordField();
 		passwordField_4.setBounds(200, 285, 263, 20);
 		contentPane.add(passwordField_4);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		URL imageURL = getClass().getResource("/main/java/Sunrise.jpg");
+		if(imageURL!=null) {
+			lblNewLabel_2.setIcon(new ImageIcon(imageURL));
+		}else {
+			System.err.println("Image file not found!");
+		}
+		lblNewLabel_2.setBounds(0, 0, 513, 383);
+		contentPane.add(lblNewLabel_2);
 	}
 }
